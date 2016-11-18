e8ecd01f295f40c9491d77c6ac2821a0bd6dff67
William Rosmon <wrosmon@Williams-MacBook-Pro.local>
2016-11-17 23:18:34 -0500

    debugging

--- Assignment7/Assignment7/Assignment7/masterWorkerIntegration.cpp
+++ Assignment7/Assignment7/Assignment7/masterWorkerIntegration.cpp
@@ -61,8 +61,8 @@ void master(double a, double b, int numPoints, double sol) {
     }
     
     while(work <= b) {
+        std::cout << "Entering main loop";
         MPI_Recv(&partialSolution, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-        std::cout << "Master process received result from " << status.MPI_SOURCE;
         sol += partialSolution;
         work += pointIncrement;
         if(work <= b) {
@@ -108,7 +108,6 @@ double function(double a, double b, int n) {
     }
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::cout << "rank " << rank << "got " << sol << "as a result\n";
     return sol;
 }
 
