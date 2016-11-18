bd4c995a6cbaae1ce6f176a074f7dfd41c02b117
William Rosmon <wrosmon@Williams-MacBook-Pro.local>
2016-11-17 23:47:29 -0500

    debugging

--- Assignment7/Assignment7/Assignment7/masterWorkerIntegration.cpp
+++ Assignment7/Assignment7/Assignment7/masterWorkerIntegration.cpp
@@ -52,9 +52,10 @@ void master(double a, double b, int numPoints, double sol) {
     MPI_Status status;
     
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Request request[size];
     
     for(rank = 1; rank < size && work <= b; rank++) {
-        MPI_Isend(&work, 1, MPI_DOUBLE, rank, 0, MPI_COMM_WORLD, 0);
+        MPI_Isend(&work, 1, MPI_DOUBLE, rank, 0, MPI_COMM_WORLD, request[i]);
         work += pointIncrement;
     }
     
