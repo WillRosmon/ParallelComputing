587e6fed8e16aaac51c544a9e144060c67ab1150
William Rosmon <wrosmon@Williams-MacBook-Pro.local>
2016-11-19 03:31:24 -0500

    Begin implementation of merge sort

--- Assignment5/Assignment5/mergeSort.cpp
+++ Assignment5/Assignment5/mergeSort.cpp
@@ -7,3 +7,37 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+#include <omp.h>
+#include <stdlib.h>
+#include <ctime>
+#include <chrono>
+
+populateArray(int*, int);
+
+int main(int argc, const char* argv[]) {
+    int arraySize = atoi(argv[1]);
+    int numThreads = atoi(argv[2]);
+    
+    if(arraySize == 0 || numThreads == 0) {
+        std::cout << "Invalid Parameters" << std::endl;
+        return 0;
+    }
+    
+    omp_set_num_threads(numThreads);
+    int* randomArray = new int[arraySize];
+    populateArray(randomArray, size);
+    
+    //clean up memory
+    delete[] randomArray;
+    randomArray = NULL;
+}
+
+//Fill the array with values of 1 - size
+void populateArray(int* array, int size) {
+    srand(time(NULL));
+#pragma omp parallel for schedule(guided)
+    for(int i = 0; i < size; i++) {
+        array[i] = (rand() % size) + 1;
+    }
+}
