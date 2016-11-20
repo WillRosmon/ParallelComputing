8b76ebd355a8ab489079747252561c60d3c1c8a3
William Rosmon <wrosmon@Williams-MacBook-Pro.local>
2016-11-20 00:46:35 -0500

    Add sample data for Assignment 9

--- Assignment7/Assignment7/Assignment7/advancedMasterWorker.cpp
+++ Assignment7/Assignment7/Assignment7/advancedMasterWorker.cpp
@@ -13,37 +13,40 @@
 
 double function(double, double, int, int, int);
 double f(double);
-void master(double, double, int);
+void master(double, double, int, double*);
 
 int main(int argc, char * argv[]) {
     double a = atof(argv[1]);
     double b = atof(argv[2]);
     int numPoints = atoi(argv[3]);
+    int initialBufferSize = atoi(argv[4]);
     
     int rank, size;
     double buff;
     double sol = 0;
-    MPI_Status status;
     MPI_Init(&argc, &argv);
     
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     
+    MPI_status status[size];
+
+    
     if(rank == 0) {
-        for(int i = 0; i < size; i++) {
-            MPI_Isend(i, 1, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
-        }
+//        for(int i = 0; i < size; i++) {
+//            MPI_Isend(i, 1, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
+//        }
+//        
+//        while(i <= size) {
+//            MPI_Recv(buff, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+//            MPI_ISend(i++, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD, 0);
+//            sol += buff;
+//        }
+//        for(int i = 0; i < size; i++) {
+//            MPI_ISend(-1, 1, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
+//        }
         
-        while(i <= size) {
-            MPI_Recv(buff, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-            MPI_ISend(i++, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD, 0);
-            sol += buff;
-        }
-        for(int i = 0; i < size; i++) {
-            MPI_ISend(-1, 1, MPI_INT, i, 0, MPI_COMM_WORLD, 0);
-        }
-        
-        master(a, b, numPoints, sol);
+        master(a, b, numPoints, &sol);
         
     } else {
         int b = 0;
@@ -62,19 +65,17 @@ int main(int argc, char * argv[]) {
     return 0;
 }
 
-double master(double a, double b, int numPoints, double sol) {
-    MPI_Status status;
+double master(double a, double b, int numPoints, double* sol) {
     int rank = 1;
     double work = a;
     double pointIncrement = (b - a) / numPoints;
     double partialSolution;
     int size;
     double initialbuffer[3];
-
+    MPI_request requests[size];
     int nextRequest = 0;
     
     MPI_COMM_size(MPI_COMM_WORLD, &size);
-    MPI_Request request[size];
     
     //Send initial 3 pieces of work to each process
     for(rank = 1; rank < size; rank++) {
@@ -93,8 +94,6 @@ double master(double a, double b, int numPoints, double sol) {
             MPI_Isend(&work, 1, MPI_DOUBLE, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &request[status.MPI_SOURCE]);
         }
     }
-    
-    
 }
 
 double function(double a, double b, int n, int start, int end) {
--- Assignment7/Assignment7/Assignment7/advancedMaster_Worker.cpp
+++ Assignment7/Assignment7/Assignment7/advancedMaster_Worker.cpp
@@ -6,4 +6,166 @@
 //  Copyright © 2016 William Rosmon. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
+#include <mpi.h>
+#include <stdlib.h>
+#include <math.h>
+
+const int WORK_ASSIGNMENT = 0;
+const int REQUEST_WORK = 1;
+const int WORK_COMPLETED = 2;
+const int TERMINATE = 3;
+
+double f(double x, int complexity) {
+    for(int i = 0; i < complexity; i++) {
+        x = pow(x, 2);
+        x = sqrt(x);
+    }
+    return x;
+};
+
+double function(double a, double b, int n, int start, int end, int complexity) {
+    //perform the integration
+    double sol = 0;
+    for(int i = start; i <= end; i++) {
+        double inside = (a + i * ( ( b - a ) / n ) ) ;
+        sol += f(inside, complexity) * ( ( b - a) / n );
+    }
+    
+    return sol;
+}
+
+void initializeBuffer(double* buffer, double a, double b, int numPoints) {
+    double increment = (b - a) / numPoints;
+    for(int i = 0; i < numPoints; i++) {
+        buffer[i] = increment * i + a;
+    }
+}
+
+double master(double a, double b, int numPoints, double* sol, int initialSize, double* buffer) {
+    int size                = MPI_COMM_size(MPI_COMM_WORLD, &size);
+    int sizeToSend          = initialSize;
+    int bufferPos           = 0;
+    double* bufferPoint     = &buffer[bufferPos];
+    int workToComplete      = numPoints;
+    MPI_Status status;
+    double partialSolution = 0;
+    double inputBuffer
+    
+    initializeBuffer(bufferPoint,a, b, numPoints);
+    MPI_Request requests[size];
+    
+    //send initial requests
+    for(int i = 1; i < size; i++) {
+        if(workToComplete == 0) {
+            break;
+        }
+        if(workToComplete >= sizeToSend) {
+            MPI_Isend(&bufferPoint, sizeToSend, MPI_DOUBLE, i, 0, &requests[i]);
+            bufferPoint = &buffer[i * sizeToSend];
+            workToComplete -= sizeToSend;
+        } else {
+            MPI_Isend(&bufferPoint, workToComplete, MPI_DOUBLE, i, 0, &requests[i]);
+            workToSend = 0;
+        }
+    }
+    while(workToComplete > 0) {
+        MPI_Recv();
+    }
+    
+    //Send termination signal to all worker processed
+    for(int i = 0; i < )
+}
+
+void incrementBufferPosition(int* position, int bufferSize) {
+    int newPosition = *position + 1;
+    *position = newPosition;
+    if( *position >= bufferSize) {
+        *position = 0;
+    }
+}
+
+void incrementRequestBuffer(int* position, int bufferSize) {
+    int newPosition = *position + 1;
+    *position = newPosition;
+    if(*position >= bufferSize) {
+        *position = 0;
+    }
+}
+
+void worker(int rank, int initialBufferSize) {
+    double* buffer = new double[initialBufferSize];
+    double* emptyBufferBegin = &buffer[0];
+    double* sendBuffer[initialBufferSize];
+    int nextWorkPosition = 0;
+    int nextSendBufferPosition = 0;
+    int emptyBufferSize = initialBufferSize;
+    int workRequestAmount = 0;
+    MPI_Status status;
+    MPI_Request requests[initialBufferSize];
+    int newRequest = 0;
+    int count;
+    bool terminate = false;
+    
+    //Receive initial payload
+    MPI_Recv(&buffer, initialBufferSize, MPI_DOUBLE, 0, WORK_ASSIGNMENT, MPI_COMM_WORLD, &status);
+    int receivedCount = MPI_GET_COUNT(&status, MPI_DOUBLE, &count);
+    emptyBuffersize = initilBufferSize - receivedCount;
+    if(emptyBufferSize > 0) {
+        if(emptyBufferSize < 10) {
+            workRequestAmount = 9;
+        } else {
+            workRequestAmount = emptyBufferSize;
+        }
+        
+        MPI_Isend(&workRequestAmount, 1, MPI_DOUBLE, 0, REQUEST_WORK, MPI_COMM_WORLD, &request[newRequest]);
+        incrementRequestBuffer(&newRequest, initialBufferSize);
+    }
+    
+    //continue processing work
+    while(!terminate) {
+        double partialSolution = function((double)work, (double)work + 1, 1);
+        sendBuffer[nextSendBufferPosition] = partialSolution;
+        incrementBufferPosition(&nextSendBufferPosition, initialBufferSize);
+        MPI_Isend(sendBuffer[nextSendBufferPosition], 1, MPI_DOUBLE, 0, WORK_COMPLETED, MPI_COMM_WORLD, &request[newRequest]);
+        incrementRequestBuffer(&newRequest, initialBufferSize);
+        
+        
+    }
+}
+
+int main(int argc, char * argv[]) {
+    double a = atof(argv[1]);
+    double b = atof(argv[2]);
+    int numPoints = atoi(argv[3]);
+    int initialBufferSize = atoi(argv[4]);
+    int rank, size, sizeToSend;
+    double* buffer = new double[numPoints];
+    double sol = 0;
+    
+    //create and populate array of random integers
+    
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    if(rank == 0) {
+        master(a, b, numPoints, &sol, initialBufferSize, buffer);
+    } else {
+        worker(rank, initialBufferSize);
+    }
+    
+    if(rank == 0) {
+        MPI_FINALIZE();
+    }
+}
+
+
+
+
+
+
+
+
+
